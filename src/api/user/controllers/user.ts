c8e43a257c96453ef66d5c36412a9d16288487f6
giangnam <legiang1199@gmail.com>
2024-09-17 17:23:18 +0700

    update

--- src/api/user/controllers/user.ts
+++ src/api/user/controllers/user.ts
@@ -1,34 +1,44 @@
 import { errors } from "@strapi/utils";
-const { NotFoundError } = errors;
+const { NotFoundError, ApplicationError } = errors;
 
 export default {
   getZaloPhoneNumber: async (ctx, next) => {
     const endpoint = "https://graph.zalo.me/v2.0/me/info";
     const secretKey = process.env.ZALO_SECRET;
+
     try {
       const userAccessToken = ctx.request.headers["access-token"];
       const token = ctx.request.headers["token"];
 
       if (!userAccessToken) {
-        throw new NotFoundError("access token not found");
+        throw new NotFoundError("Access token not found");
       }
 
       if (!token) {
-        throw new NotFoundError("token not found");
+        throw new NotFoundError("Token not found");
       }
 
       const response = await fetch(endpoint, {
         method: "GET",
         headers: {
-          ContentTypes: "application/json",
+          "Content-Type": "application/json",
           access_token: userAccessToken,
           code: token,
           secret_key: secretKey,
         },
       });
-      ctx.body = await response.json();
+
+      // Check for successful response
+      if (!response.ok) {
+        throw new ApplicationError(
+          `Failed to fetch Zalo info: ${response.statusText}`,
+        );
+      }
+
+      const data = await response.json();
+      ctx.body = data;
     } catch (err) {
-      ctx.body = err;
+      ctx.throw(400, err.message);
     }
   },
 };
